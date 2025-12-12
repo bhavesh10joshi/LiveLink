3ca52c35b48d717133e7f29cf5d903b847ab7360
bhavesh joshi <joshbhavesh10@gmail.com>
2025-12-12 20:28:59 +0530

    Started Writing Backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,47 +1,110 @@
-import { WebSocketServer , WebSocket} from "ws";
+import express, { NextFunction }  from "express";
+import bcrypt from "bcrypt" ; 
+import { z } from "zod" ; 
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import { Request , Response } from "express";
 
-//new connection 
-const wss = new WebSocketServer({port : 8080});
+const mongodbUrl:string = ""; 
+mongoose.connect(mongodbUrl);
 
-//storing users
-interface UserType {
-    socket : WebSocket , 
-    RoomNo : string
-};
-const Users : UserType[] = [];
-//connection established
-wss.on("connection" , function(socket){
-    socket.on("message" , function(e)
+const USER_SECRET:string = "iambhaveshjoshi";
+
+const App = express();
+App.use(express.json());
+
+//zod validation 
+const NewZodobject = z.object({
+    username : z.string().min(8).regex(/[0-9]/ , { message : "The given username should contain atleast one Number !"})
+                                .regex(/[A-Z]/ , {message : "The given username should contain atleast one Capital Letter !"})
+                                .regex(/[a-z]/ , {message : "The username should contain atleast one Small Alphabet !"}) ,
+    password : z.string().min(8).regex(/[0-9]/ , { message : "The given password should contain atleast one Number !"})
+                                .regex(/[A-Z]/ , {message : "The given password should contain atleast one Capital Letter !"})
+                                .regex(/[a-z]/ , {message : "The password should contain atleast one Small Alphabet !"}) ,
+}); 
+//type inference in zod 
+type ZodObject = z.infer<typeof NewZodobject>;
+
+//SignUp EndPoint
+App.post("/User/SignUp" , async function(req:Request , res:Response)
+{
+    const Signup:ZodObject = req.body;
+    const Zodsafeobject = NewZodobject.safeParse(req.body);
+    
+    if(Zodsafeobject)
     {
-        const Message:any = JSON.stringify(e.toString());
-        //users can join a room 
-        if(Message.type == "join")
-        {
-            Users.push({
-                socket : socket , 
-                RoomNo : Message.payload.roomId
+        //hashing the password by the user 
+        const Hashedpassword = bcrypt.hash(Signup.password , 5);
+        try{
+            await UserRouter.create({
+                username : Signup.username , 
+                password : Signup.password
             });
-        }//users can c761756699976175
-        else if(Message.type === "chat")
-        {
-            let CurrentUserRoom = null;
-            for(let i = 0 ; i<Users.length ; i++)
-            {
-                if(Users[i]?.socket === socket)
-                {
-                    CurrentUserRoom = Users[i]?.RoomNo;
-                }
-            }
-            if(CurrentUserRoom != null)
-            {
-                for(let i = 0 ; i < Users.length ; i++)
-                {
-                    if(Users[i]?.RoomNo === CurrentUserRoom)
-                    {   
-                        socket.send(Message.payload.message);
-                    }
-                }
-            }
+            res.status(200).json({
+                msg : "Signed Up Successfully !"
+            });
         }
+        catch(e)
+        {
+            res.status(404).json({
+                msg : "Internal Server error !"  
+            });
+        }
+    }
+    else
+    {
+        res.status(404).json({
+            msg : "The given Credentials are not correct !"
+        });
+    }
+}); 
+App.post("/User/SignIn" , async function(req:Request , res:Response)
+{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const find = await UserRouter.findOne({
+        username : username  
     });
+
+    if(find)
+    {
+        const check = await bcrypt.compare(password , find.password);
+        if(check)
+        {
+            const token = jwt.sign({
+                id : find._id
+            } , USER_SECRET);
+            res.status(200).json({
+                token : token
+            });
+        }
+        else
+        {
+            res.status(404).json({
+                msg : "Incorrect Password !"
+            });
+        }
+    }
+    else
+    {
+        res.status(404).json({
+            msg : "The Username provided does not exist "
+        });
+    }
 });
+//middleware
+function middleware(req:Request , res:Response , next:NextFunction)
+{
+    const token:any = req.headers["authorization"];
+    const check = jwt.verify(token , USER_SECRET);
+    if(check)
+    {
+        
+    }
+    else
+    {
+
+    }
+}
+App.listen(3000);
